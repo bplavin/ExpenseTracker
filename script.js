4546f8681a00a9aaacba30ddf4745eb81133f91c
Bogdan Plavin <bohdan.plavin@gmail.com>
2021-06-14 12:13:03 -0500

    DOM elements appears dynamically

--- script.js
+++ script.js
@@ -14,3 +14,29 @@ const randomTransactions = [
 ];
 
 let transactions = randomTransactions;
+
+// Add transaction to DOM list 
+function addTransactionDOM(transaction) {
+    // Get sign
+    const sign = transaction.amount < 0 ? '-' : '+';
+
+    const item = document.createElement('li');
+
+    //Add class based on value 
+    item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+
+    item.innerHTML = `
+    ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span><button class='delete-btn'>x</button>
+    `;
+
+    list.appendChild(item);
+};
+
+// Init app 
+function init() {
+    list.innerHTML = '';
+
+    transactions.forEach(addTransactionDOM);
+}
+
+init();
