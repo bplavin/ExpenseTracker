595b9a00d48402cd87bd02f1b7b2a11617d32518
Bogdan Plavin <bohdan.plavin@gmail.com>
2021-06-14 12:39:44 -0500

    Functions that display balance and counts money

--- script.js
+++ script.js
@@ -32,11 +32,34 @@ function addTransactionDOM(transaction) {
     list.appendChild(item);
 };
 
+// Update the balance, icome and expense
+function updateValues() {
+    const amounts = transactions.map(transaction =>
+        transaction.amount);
+
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+    const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc,item) => (acc += item), 0)
+    .toFixed(2);
+
+    const expense = (amounts
+        .filter(item => item < 0)
+        .reduce((acc, item) => (acc += item), 0) * -1)
+        .toFixed(2);
+        
+    balance.innerText = `$${total}`;
+    money_minus.innerText = `$${expense}`;
+    money_plus.innerText = `$${income}`;
+}
+
 // Init app 
 function init() {
     list.innerHTML = '';
 
     transactions.forEach(addTransactionDOM);
+    updateValues();
 }
 
 init();
